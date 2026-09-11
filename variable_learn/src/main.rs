a3ea060a360242f124d3a601011436180031d3b8
MSH-Cyber <honoresewanoude@gmail.com>
2026-09-11 23:32:13 +0200

    Initial commit

---
+++ variable_learn/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut mon_age = 23;
+    println!("{mon_age}");
+
+    mon_age = 24;
+    println!("J'ai changé mon age et c'est {}", mon_age);
+
+    let mon_nom = "Honoré";
+    println!("Je m'appelle {} ", mon_nom)
+}

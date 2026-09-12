105576ec0d2b08982795318732762eee8aa94b86
MSH-Cyber <honoresewanoude@gmail.com>
2026-09-13 00:22:27 +0200

    lecture des entrer

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main(){
+
+   
+    println!("Mon premier jeu de devinette");
+    println!("Entrer votre valeur");
+
+    let mut dev = string::new();
+
+
+    io::stdin()
+        .read_line(&mut dev);
+
+    println!("Votre nombre deviner est : {} ", dev);
 }

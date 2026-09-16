e41b6e60e2817e71b948e949de42114dbd3449da
MSH-Cyber <honoresewanoude@gmail.com>
2026-09-16 04:10:12 +0200

    Utlisation de parse(), trim(), loop(), du Ordering avec ces branche 'arms', du cmp(), et du match avec ces pattern

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,22 +1,46 @@
-use std::io;
+use std::{cmp::Ordering, io};
 use rand::Rng;
+
 
 fn main(){
 
    
     println!("Mon premier jeu de devinette");
+
+    
+
+    
     println!("Entrer votre valeur");
 
     let nombre_secret = rand::thread_rng().gen_range(1..=100);
 
+    
+
+    //let dev: u32 = dev.trim().parse().expect("S'il vous plait saisissez un nombre");
+
+    loop {
+
     let mut dev = String::new();
-
-    println!("Le nombre secret est : {}",nombre_secret);
-
-
+    
     io::stdin()
         .read_line(&mut dev)
         .expect("Erreur de lecture");
 
+    let dev: u32 = match dev.trim().parse(){
+        Ok(num) => num,
+        Err(_) => continue,
+    };
+
+
     println!("Votre nombre deviner est : {} ", dev);
+
+    match dev.cmp(&nombre_secret){
+        
+        Ordering::Equal => {println!("Vous avez trouvé la bonne reponse");break},
+        Ordering::Greater => println!("Le nombre que vous avez chois est trop grand"),
+        Ordering::Less => println!("Le nombre que vous avez choisi est trop petit"),
+
+    }
+
+}
 }
